ef162bb3b20b086d2d09dbc612ab681bc7b66fbd
Jay <joflo1@gmx.de>
2025-10-22 11:57:35 +0200

    Menu Handler

--- src/main/java/org/example/menu/MenuHandler.java
+++ src/main/java/org/example/menu/MenuHandler.java
@@ -1,6 +1,60 @@
-package menu;
+package org.example.menu;
+
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MenuHandler {
+    public static void main(String[] args) {
+        // Hauptfenster erstellen
+        JFrame frame = new JFrame("Spiel-Menü");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
 
+        // Menüleiste erstellen
+        JMenuBar menuBar = new JMenuBar();
 
+        // Menüpunkte erstellen
+        JMenu spielMenu = new JMenu("Spiel");
+        JMenu einstellungenMenu = new JMenu("Einstellungen");
+        JMenu hilfeMenu = new JMenu("Hilfe");
+
+        // Untermenüpunkte für "Spiel" erstellen
+        JMenuItem neuesSpielItem = new JMenuItem("Neues Spiel");
+        JMenuItem beendenItem = new JMenuItem("Beenden");
+
+        // Untermenüpunkte zu "Spiel" hinzufügen
+        spielMenu.add(neuesSpielItem);
+        spielMenu.addSeparator();
+        spielMenu.add(beendenItem);
+
+        // Untermenüpunkte für "Einstellungen" erstellen
+        JMenuItem schwierigkeitItem = new JMenuItem("Schwierigkeit");
+        einstellungenMenu.add(schwierigkeitItem);
+
+        // Untermenüpunkte für "Hilfe" erstellen
+        JMenuItem anleitungItem = new JMenuItem("Anleitung");
+        hilfeMenu.add(anleitungItem);
+
+        // Menüpunkte zur Menüleiste hinzufügen
+        menuBar.add(spielMenu);
+        menuBar.add(einstellungenMenu);
+        menuBar.add(hilfeMenu);
+
+        // Aktionen für Menüpunkte definieren
+        beendenItem.addActionListener(e -> {
+            System.exit(0);
+        });
+
+        neuesSpielItem.addActionListener(e -> {
+            JOptionPane.showMessageDialog(frame, "Neues Spiel gestartet!");
+            // Hier Logik für neues Spiel einfügen
+        });
+
+        // Menüleiste zum Frame hinzufügen
+        frame.setJMenuBar(menuBar);
+
+        frame.setVisible(true);
+    }
 }
